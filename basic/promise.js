47f307a50db19d0df98d11bee2575ac9e5c7fbc4
Sagor <mbrsagor@gmail.com>
2020-08-26 09:54:27 +0600

    promise in javascript

---
+++ basic/promise.js
@@ -0,0 +1,10 @@
+var promise = new Promise(function (resolve, reject) {
+    resolve(1);
+});
+
+promise.then(function (val) {
+    console.log(val); // 1
+    return val + 2;
+}).then(function (val) {
+    console.log(val); // 3
+});
