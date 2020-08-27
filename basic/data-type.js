7e1f480182fe2807c014bc3c1fa80735ef7f2fa1
Sagor <mbrsagor@gmail.com>
2020-08-27 08:22:26 +0600

    Basic datatype example

--- basic/data-type.js
+++ basic/data-type.js
@@ -5,19 +5,65 @@ kind of javascript data type:
 3) Boolean
 4) Undefine
 5) Null
+6) object
 */
 
+var name = "Md.Bozlur Rosid Sagor";
+console.log(name);
 
-// const radious = 7;
-// const pi = 3.474774;
-// const areaOfCircule = pi * (radious * radious);
-// console.log(areaOfCircule);
+let address = "Dhormopur sundorgonj, Gaibandha";
+console.log(address);
 
-// let BDT = 83;
-// let totalBDT = 5500;
-// let DOLLER = totalBDT / BDT;
-// console.log("Total USD $", DOLLER);
+let_var = () => {
+    name = "mbr-sagor";
+    address = "Dhaka Bangladesh";
 
-// let num = 5;
-// num++;
-// console.log(num);
+    console.log(`My nickname is ${name}\nI live in ${address}`);
+}
+
+// let_var();
+
+var num = 20;
+let num2 = 30;
+
+console.log(num);
+console.log(num + num2);
+
+sumNum = (n1, n2) => {
+    console.log(`Total sum is: ${n1 + n2}`);
+}
+
+// sumNum(30, 70);
+
+let is_active = true;
+if (is_active === true) {
+    console.log("I'm active user.");
+} else if (is_active === false || is_active === true) {
+    console.log("I'm not regular and active user.");
+} else {
+    console.log("I'm not active user yet.");
+}
+
+let username;
+console.log(username);
+username = "mbrsagor";
+console.log(username);
+
+myusername = () => {
+    username = 'mbr-sagor-bd';
+    console.log(username);
+}
+
+myusername();
+
+email = null;
+console.log(email);
+
+email = "mbrsagor@gmail.com";
+console.log(email);
+
+var data = {
+    "username": "sagor",
+    "password": "p-123"
+};
+console.log(data);
