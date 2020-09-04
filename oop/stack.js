037ae414615fbb1a17eec0dc048d91fd30f61979
Sagor <mbrsagor@gmail.com>
2020-09-04 11:30:28 +0600

    Basic completed stack example in ES6 mode

--- oop/stack.js
+++ oop/stack.js
@@ -8,11 +8,27 @@ class Stack {
         this.items.push(element);
     }
 
+    // Remove item from stack
+    removeItem = () => {
+        return this.items.pop();
+    }
+
     // Print all item
     printItem = () => {
         for (let item = 0; item < this.items.length; item++) {
             const element = this.items[item];
             console.log(element);
+        }
+    }
+
+    // Size of item
+    itemSize = () => {
+        console.log(`Item size of: ${this.items.length}`);
+    }
+
+    isEmpty = () => {
+        if (this.items.length === 0) {
+            console.log(`Sorry! there area no item in the array.`)
         }
     }
 
@@ -24,4 +40,9 @@ my_stack.addItem("MacBook");
 my_stack.addItem("MacBook-Air");
 my_stack.addItem("MacBook-Pro");
 
+my_stack.printItem();
+my_stack.itemSize();
+console.log("\n");
+// Remove item function call
+my_stack.removeItem();
 my_stack.printItem();
