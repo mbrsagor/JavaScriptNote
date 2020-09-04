47052645ae9c66327412f3df9f64619e99cedf4f
Sagor <mbrsagor@gmail.com>
2020-09-04 10:54:42 +0600

    Dict developer folder delete from function directory

--- functions/dist/callback.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-setTimeout(function () {
-  console.log("This message is shown after 3 seconds");
-}, 3000);
-var title = "Hello, I'm learning callback function with sagor";
-console.log(title);
