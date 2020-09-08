abab0e94fe680d21cfdc61e327b3e23e76dc029a
Sagor <mbrsagor@gmail.com>
2020-09-09 03:39:59 +0600

    Arrow function

--- functions/mosh.js
+++ functions/mosh.js
@@ -34,4 +34,35 @@ person2.walk();
 const walk = person2.walk.bind(person2);
 console.log(walk);
 walk()
- 
+
+
+// Function and Arrow function both example
+const squre = function (number) {
+    return number * number;
+}
+console.log(squre(2))
+
+// If you use simple paramiter you may not use bracket.
+add = number => number * number;
+console.log(add(2));
+
+
+const jobs = [{
+        id: 1,
+        isActive: true
+    },
+    {
+        id: 2,
+        isActive: false
+    },
+    {
+        id: 2,
+        isActive: true
+    },
+];
+
+const active_job = jobs.filter(function (job) { return job.isActive }) // Without arrow function
+console.log(active_job);
+
+const active_job2 = jobs.filter(job => job.isActive) // Arrow function
+console.log(active_job2);
