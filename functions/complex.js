026fc39770ee3000fc1f75263c0500546270340e
Sagor <mbrsagor@gmail.com>
2020-09-09 13:00:08 +0600

    Inner and higher order function

--- functions/complex.js
+++ functions/complex.js
@@ -12,3 +12,21 @@ function greet(fmMessage) {
 }
 greet(aboutMe);
 greet(aboutMe());
+
+// Inner function
+function saySomething() {
+    return function () {
+        return "HEllo world";
+    }
+}
+
+let fn = saySomething();
+let message = fn();
+console.log(message);
+
+// Higher order function
+let numbers = [1, 2, 3, 4, 5]
+let fnc= numbers.map(number => number * 2);
+console.log(fnc);
+
+setTimeout(() => console.log(`Hello, I'm set time out function...`), 1000);
