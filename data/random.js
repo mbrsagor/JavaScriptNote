ca9d09d4420a10557268eab502feca5abdc0a32a
Sagor <mbrsagor@gmail.com>
2020-09-19 01:50:48 +0600

    Random variable

---
+++ data/random.js
@@ -0,0 +1,29 @@
+// Multiple varialbe use
+let a = b = c = 30
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log('\n');
+
+// One Statement, Many Variables
+let x = 10, y = 20, z = 30;
+console.log(x);
+console.log(y);
+console.log(z);
+
+// Undefined
+let username;
+console.log(username);
+username = "mbr-sagor";
+console.log(username);
+
+// Null
+let is_ative = null;
+console.log(is_ative);
+is_ative = true;
+console.log(is_ative);
+
+var $ = "username";
+console.log($);
+
