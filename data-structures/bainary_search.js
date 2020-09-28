2764b225fc3a81529fe56b5a7afbcbc8f297f082
Sagor <mbrsagor@gmail.com>
2020-09-28 16:45:46 +0600

    Binary search well structure

--- data-structures/bainary_search.js
+++ data-structures/bainary_search.js
@@ -1,20 +1,26 @@
-let a = [1, 2, 4, 6, 1, 100, 0, 10000, 3];
+let binarySearch = function (arr, val) {
+    var half = Math.floor(arr.length / 2);
 
-a.sort(function (a, b) {
-    return a - b;
+    if (arr.length === 0) {
+        return -1;
+    } else if (arr[half] === val) {
+        console.log("val: ", val, "arr[half]: ", arr[half]);
+        return val;
+    } else if (val > arr[half]) {
+        console.log("val: ", val, "arr[half]: ", arr[half]);
+        return binarySearch(arr.slice(half, arr.length), val);
+    } else {
+        console.log("val: ", val, "arr[half]: ", arr[half]);
+        return binarySearch(arr.slice(0, half), val);
+    }
+}
+
+
+let arr = [1, 5, 20, 58, 76, 8, 19, 41].sort(function (a, b) {
+    return a - b
 });
 
-function binarySearch(arr, value) {
-    if (arr.length === 0) return -1;
-    var low = 0,
-        high = arr.length - 1,
-        mid;
-    while (low <= high) {
-        mid = Math.floor((low + high) / 2);
-        if (arr[mid] == value) return mid;
-        else if (arr[mid] < value) low = mid + 1;
-        else high = mid - 1;
-    }
-    return -1;
-}
-console.log(binarySearch(a, 1005))
+console.log("Sorted array: " + arr);
+console.log(binarySearch(arr, 76));
+console.log(binarySearch(arr, 19));
+console.log(binarySearch(arr, 0));
