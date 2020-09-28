048c4cf4f3596c6e8e71df748fc9a9ea01cb38fb
Sagor <mbrsagor@gmail.com>
2020-09-28 16:57:31 +0600

    Simple function with paramitter added

--- data/lamda.js
+++ data/lamda.js
@@ -6,3 +6,7 @@ const materials = [
 ];
 
 console.log(materials.map(material => material.length));
+
+// Simple array function
+say_hello = name => `Hello ${name} how about you?`;
+console.log(say_hello('Sagor'));
