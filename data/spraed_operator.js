7b60c1f46b2f618c72e064ac2d3bddcbaa82d838
Sagor <mbrsagor@gmail.com>
2020-10-07 15:18:23 +0600

    Work on inner function and outer function

--- data/spraed_operator.js
+++ data/spraed_operator.js
@@ -2,3 +2,20 @@ const users = ['sagor', 'ohi', 'apple'];
 const all_user = [...users, 'shanto', 'showrove'];
 
 console.log(all_user);
+
+// Inner and outer function
+var b = 1;
+
+function outer() {
+    var b = 2;
+
+    function inner() {
+        b++;
+        b = 3;
+        console.log(b);
+    }
+
+    inner();
+}
+
+outer();
