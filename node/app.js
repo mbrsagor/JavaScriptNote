92172431793006536bf37c8a6ad2b2e64238727a
Sagor <mbrsagor@gmail.com>
2020-11-01 00:13:26 +0600

    Path module how to worked

---
+++ node/app.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const my_path = '/Users/macair/Documents/practice/javaScript/ES6/node/app.js';
+
+console.log(path.basename(my_path));
+console.log(path.dirname(my_path));
+console.log(path.extname(my_path));
+console.log(path.parse(my_path));
