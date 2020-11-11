8c9e7aaa5e41e528c57e8f61c309fb30211038ad
Sagor <mbrsagor@gmail.com>
2020-11-11 23:20:41 +0600

    Rest parameter

---
+++ functions/restParamitter.js
@@ -0,0 +1,16 @@
+function numList(...names) {
+    console.log(names);
+}
+
+numList("Sagor", "Nodi", "Khal", "Bill", "Etc");
+
+// Calculator
+function calculator(...numbers) {
+    let sum = 0;
+    for (let num of numbers) {
+        sum += num;
+    }
+    console.log(`Total calculate number of result: ${sum}`);
+}
+
+calculator(20, 30, 50);
