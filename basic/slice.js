79d1626235728e37077ec2470139ad75b332ce20
Sagor <mbrsagor@gmail.com>
2020-12-05 23:21:32 +0600

    Slice simple example

---
+++ basic/slice.js
@@ -0,0 +1,8 @@
+let fruiletts = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+let citrus = fruiletts.slice(1, 3);
+console.log(citrus);
+console.log(fruiletts);
+
+let languages = ['python', 'javascript', 'dart', 'swift', 'HTML', 'CSS']
+let lang = languages.slice(0, 2);
+console.log(lang);
