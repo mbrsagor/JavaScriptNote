59fde97af34e34e0a81f020cd20509b35bbb8bfb
Sagor <mbrsagor@gmail.com>
2021-01-05 13:19:24 +0600

    Basic javascrpt dom

---
+++ components/dom/index.js
@@ -0,0 +1,9 @@
+document.getElementById("demo").innerHTML = "I'm full-stack software engineer.";
+
+// On click evnet
+funcOnAlert.onclick = () => {
+    let x = 20, y = 30;
+    alert(`Total sum result is: ${x + y}`);
+};
+
+// Form submit event
