19ad5d180b2d0c5500339284a2c29211a101066e
mbrsagor <mbrsagor@gmail.com>
2021-02-05 01:11:23 +0600

    Array common operations

--- data/index.js
+++ data/index.js
@@ -1,9 +1,18 @@
 let book = { title: "Simple blog post title" }
 
 function publish(book) {
-    book.isPublish = true;
-    // book.set("IsPublish", true);
+    book.isPublish = false;
 }
 
 publish(book);
 console.log(book);
+
+const array1 = [5, 12, 8, 130, 44];
+const isLargeNumber = (element) => element > 13;
+console.log(array1.findIndex(isLargeNumber));
+
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+console.log(beasts.indexOf('bison'));
+console.log(beasts.lastIndexOf('duck'))
+console.log(beasts.sort());
+console.log(beasts.reverse());
