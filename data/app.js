3b6ac1d5974c0ad9c6c3427b00f47efd49bc6d80
mbrsagor <mbrsagor@gmail.com>
2021-02-20 02:28:31 +0600

    Editor setup

---
+++ data/app.js
@@ -0,0 +1,2 @@
+console.log('hello dev');
+console.log('hello develope....');
--- data/index.js
+++ data/index.js
@@ -1,4 +1,4 @@
-let book = { title: "Simple blog post title" }
+const book = { title: 'Simple blog post title' };
 
 function publish(book) {
     book.isPublish = false;
@@ -13,6 +13,6 @@ console.log(array1.findIndex(isLargeNumber));
 
 const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
 console.log(beasts.indexOf('bison'));
-console.log(beasts.lastIndexOf('duck'))
+console.log(beasts.lastIndexOf('duck'));
 console.log(beasts.sort());
 console.log(beasts.reverse());
