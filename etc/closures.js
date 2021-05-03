a8dbaea8baca74f865ed21eeb7ed84c9b2daafe1
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2021-05-03 21:48:36 +0600

    Update closures.js

--- etc/closures.js
+++ etc/closures.js
@@ -8,3 +8,21 @@ function makeFunc() {
 
 const myFunc = makeFunc();
 myFunc();
+
+// Exmple 2
+function outer() {
+var b = 10;
+   function inner() {
+        
+         var a = 20; 
+         console.log(a+b);
+    }
+   return inner;
+}
+var X = outer(); 
+var Y = outer(); 
+//end of outer() function executions
+X(); // X() invoked the first time
+X(); // X() invoked the second time
+X(); // X() invoked the third time
+Y(); // Y() invoked the first time
