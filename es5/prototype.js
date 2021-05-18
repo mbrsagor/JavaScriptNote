293c49c97d4deeadc0e4cef32d68e4e097fb8117
mbrsagor <mbrsagor@gmail.com>
2021-05-18 23:31:07 +0600

    Proto Type updated

--- es5/prototype.js
+++ es5/prototype.js
@@ -1,18 +1,25 @@
-var teacher = function(){
+const teacher = function () {};
 
-}
+teacher.get_teacher.name = 'Default name';
+teacher.get_teacher._subject = 'Default subject';
+teacher.get_teacher.department = 'Default department';
 
-teacher.get_teacher.name = "Default name";
-teacher.get_teacher._subject = "Default subject";
-teacher.get_teacher.department = "Default department";
+teacher.get_teacher.get_all = function () {
+    console.log(
+        `Teacher Name: ${this.name}\n` + `Subject: ${this.subject}Department: ${this.department}\n`
+    );
+};
 
-teacher.get_teacher.get_all = function(){
-	console.log("Teacher Name: "+this.name+"\n"+"Subject: "+this.subject+"Department: "+this.department+"\n");
-}
+const teacher1 = new teacher();
+teacher1.name = 'Sadia sultana';
+teacher1._subject = 'CSE';
+teacher1.department = 'computer science & engineering';
 
-var teacher1 = new teacher()
-teacher1.name = "Sadia sultana";
-teacher1._subject = "CSE";
-teacher1.department = "computer science & engineering";
+teacher1.get_all();
 
-teacher1.get_all()
+
+class car = function () {};
+
+car.name = "New BMW";
+car.price = 111500;
+car.cc = 1200
