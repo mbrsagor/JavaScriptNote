5be10eff6e46e8fb5ae56c03c1b555892266b94f
mbrsagor <mbrsagor@gmail.com>
2021-06-30 22:17:13 +0600

    New: String replace and replace-all

---
+++ basic/string.js
@@ -0,0 +1,11 @@
+const text = 'I love React and Angular.JS';
+console.log(text);
+
+console.log(text.replace('React', 'React.JS'));
+
+// Example 2
+const str = 'JS will, JS will rock you!';
+console.log(str);
+
+const newStr = str.replace(/JS/g, 'JavaScript');
+console.log(newStr);
