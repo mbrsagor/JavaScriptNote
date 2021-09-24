fd46a85119ff8e9d8a10f983e83e0c5e4b37f70e
mbrsagor <mbrsagor@gmail.com>
2021-09-24 23:12:50 +0600

    New: seperate variable file

---
+++ basic/variable.js
@@ -0,0 +1,16 @@
+var address = "Dhormopur, Sundorgonj, Gaibandha";
+console.log(address);
+
+let name = "Mbr-Sagor";
+console.log(name);
+
+const username = "Sagor";
+console.log(username);
+
+// let language ="Bozlur ";
+// let middle_name = "Rosid ";
+// let last_name ="Sagor";
+// console.log(first_name, middle_name, last_name);
+
+let [first_name, middle_name, last_name] = ["Bozlur", "Rosid", "Sagor"];
+console.log(first_name, middle_name, last_name);
