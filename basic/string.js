81e61fb4a51119ffc2f8486b95e2646f4408753f
mbrsagor <mbrsagor@gmail.com>
2022-09-08 15:44:04 +0600

    New: Basic refactor.

--- basic/string.js
+++ basic/string.js
@@ -9,3 +9,21 @@ console.log(str);
 
 const newStr = str.replace(/JS/g, 'JavaScript');
 console.log(newStr);
+
+
+var address = "Dhormopur, Sundorgonj, Gaibandha";
+console.log(address);
+
+let name = "Mbr-Sagor";
+console.log(name);
+
+const username = "Sagor";
+console.log(username);
+
+// let language ="Bozlur ";
+// let middle_name = "Rosid ";
+// let last_name ="Sagor";
+// console.log(language, middle_name, last_name);
+
+let [first_name, middle_name, last_name] = ["Bozlur", "Rosid", "Sagor"];
+console.log(first_name, middle_name, last_name);
