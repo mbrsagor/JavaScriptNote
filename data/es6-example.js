7fd526ca07a9e7647edc7880c0acbdde94ec0121
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2023-06-29 10:25:36 +0600

    ES6 simple example

---
+++ data/es6-example.js
@@ -0,0 +1,7 @@
+const q1 = ["Jan", "Feb", "Mar"];
+const q2 = ["Apr", "May", "Jun"];
+const q3 = ["Jul", "Aug", "Sep"];
+const q4 = ["Oct", "Nov", "May"];
+
+const year = [...q1, ...q2, ...q3, ...q4];
+console.log(year);
