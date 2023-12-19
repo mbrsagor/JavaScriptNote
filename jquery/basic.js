6612f4207790b17e9fd976dfbbe9b944c38f90e2
mbrsagor <mbrsagor@gmail.com>
2023-12-19 13:39:31 +0600

    New: Basic event lisenser.

---
+++ jquery/basic.js
@@ -0,0 +1,11 @@
+$("p").on({
+    mouseenter: function(){
+      $(this).css("background-color", "lightgray");
+    },
+    mouseleave: function(){
+      $(this).css("background-color", "lightblue");
+    },
+    click: function(){
+      $(this).css("background-color", "yellow");
+    }
+  });
