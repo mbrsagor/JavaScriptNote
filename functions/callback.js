f32e80e10f81f9aa38eb5d686bcfe9473527d447
mbrsagor <mbrsagor@gmail.com>
2024-01-24 22:42:50 +0600

    New: simple session timeout example.

--- functions/callback.js
+++ functions/callback.js
@@ -4,3 +4,17 @@ setTimeout(function () {
 
 const title = "Hello, I'm learning callback function with sagor";
 console.log(title);
+
+
+
+let startTime = new Date(milliseconds),
+timeoutLength = 60000;
+
+let interval = setInterval("checkTimeout()",1000);
+function checkTimeout() {
+    let currentTime = new Date(millisecond);
+    if (currentTime > startTime + timeoutLength) {
+        clearInterval(interval);
+        console.log ("Your current Session is over due to inactivity.");
+    }
+}
