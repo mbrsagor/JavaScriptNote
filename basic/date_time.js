d5cb72b588183977fdaeb9eea405405d1ecd010d
mbrsagor <mbrsagor@gmail.com>
2024-04-22 22:18:59 +0600

    New: added local date example.

--- basic/date_time.js
+++ basic/date_time.js
@@ -49,3 +49,14 @@ function secondsToHms() {
 }
 
 console.log(secondsToHms());
+
+
+// Get local date
+function ToLocalDate (inDate) {
+  var date = new Date();
+  date.setTime(inDate.valueOf() - 60000 * inDate.getTimezoneOffset());
+  return date;
+}
+
+var result = ToLocalDate(1677069788);
+console.log(result);
--- basic/datetime.js
+++
@@ -1,8 +0,0 @@
-function ToLocalDate (inDate) {
-    var date = new Date();
-    date.setTime(inDate.valueOf() - 60000 * inDate.getTimezoneOffset());
-    return date;
-}
-
-var result = ToLocalDate(1677069788);
-console.log(result);
