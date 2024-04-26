886638fa87d58281fa95ea65bde12846fbc5c545
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2024-04-26 22:00:02 +0600

    New: added firestore realtime config chat

---
+++ node/firestore.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZJ4IzV9vXyLdBT-m8nEAB81Exz1LY3Ho",
+    authDomain: "mynewchat-6c1a2.firebaseapp.com",
+    projectId: "mynewchat-6c1a2",
+    storageBucket: "mynewchat-6c1a2.appspot.com",
+    messagingSenderId: "348556921164",
+    appId: "1:348556921164:web:880cb2506e14b0028667e9"
+  };
+firebase.initializeApp(firebaseConfig);
+  
+const db = firebase.database();
