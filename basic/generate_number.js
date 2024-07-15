fe1bc4f6a894685d29d6932b3c531a4bc0f6048a
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2024-07-15 23:50:37 +0600

    New: Random generate number

---
+++ basic/generate_number.js
@@ -0,0 +1,12 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+console.log(getRandomInt(3));
+// Expected output: 0, 1 or 2
+
+console.log(getRandomInt(1));
+// Expected output: 0
+
+console.log(Math.random());
+// Expected output: a number from 0 to <1
