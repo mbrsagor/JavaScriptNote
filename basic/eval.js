ca601becfb49961e44edff2d7249212147da3288
mbrsagor <mbrsagor@gmail.com>
2024-08-03 12:46:32 +0600

    New: Simple eval example.

---
+++ basic/eval.js
@@ -0,0 +1,11 @@
+console.log(eval('2 + 2'));
+// Expected output: 4
+
+console.log(eval(new String('2 + 2')));
+// Expected output: 2 + 2
+
+console.log(eval('2 + 2') === eval('4'));
+// Expected output: true
+
+console.log(eval('2 + 2') === eval(new String('2 + 2')));
+// Expected output: false
