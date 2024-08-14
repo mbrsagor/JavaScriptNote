733693ef6f351314bce6ed9b3b82fcdd047ea1b9
mbrsagor <mbrsagor@gmail.com>
2024-08-14 23:28:09 +0600

    New: make function to the code-test.

--- basic/code_test.js
+++ basic/code_test.js
@@ -1,9 +1,13 @@
-var arr1 = "john".split('');
-var arr2 = arr1.reverse();
-var arr3 = "jones".split('');
+function code_test() {
+    var arr1 = "john".split('');
+    var arr2 = arr1.reverse();
+    var arr3 = "jones".split('');
 
 arr2.push(arr3);
 
+console.log("array 1: length", arr1.length + "last=", arr1.slice(-1));
+console.log("array 2: length", arr2.length + "last=", arr2.slice(-1));
+}
 
-console.log("array 1: lengh", arr1.length + "last=", arr1.slice(-1));
-console.log("array 2: lengh", arr2.length + "last=", arr2.slice(-1));
+
+code_test();
