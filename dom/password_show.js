fdbc4d04c67c0a7a0b7687db24a31bc9b4076383
mbrsagor <mbrsagor@gmail.com>
2024-10-24 22:51:18 +0600

    New: password show hide function added.

---
+++ dom/password_show.js
@@ -0,0 +1,10 @@
+// show password 
+        function togglePassword() {
+            const passwordField = document.getElementById("password");
+            if (passwordField.type === "password") {
+                passwordField.type = "text";
+            } else {
+                passwordField.type = "password";
+            }
+        }
+
